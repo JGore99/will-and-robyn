8368c65d977007d59c3ffd8fc4664aa97f8c0e9c
Jeff_Gore <JefGore@gmail.com>
2022-05-16 18:08:26 -0400

    photo grid roughed in

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import styles from './Landing.module.css'
+import and from '../../images/and.webp'
+import rW01 from '../../images/FullSizeR (2).jpg'
+import rW02 from '../../images/FullSizeR (3).jpg'
+import rW03 from '../../images/FullSizeR (4).jpg'
 
 const Landing = () => {
   return (
     <div className={styles.landingContainer}>
-      Landing Page
-      <button className='button-bg'>
-        RSVP
-      </button>
+      <div className={styles.landingText}>
+        <h2>WILL</h2>
+        <img src={and} alt="and" />
+        <h2>ROBYN</h2>
+      </div>
+      <div className={styles.landingImageGrid}>
+        <img className={styles.landingImageLg} src={rW02} alt="photo02" />
+        <img className={styles.landingImagesm01} src={rW03} alt="photo03" />
+        <img className={styles.landingImagesm02} src={rW01} alt="photo01"/>
+      </div>
+        <button className='button-bg'>
+          RSVP
+        </button>
     </div>
   )
 }
