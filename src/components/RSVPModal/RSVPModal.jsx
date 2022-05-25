31ea62a5895aed1adf2d49494e3b25011197e46b
Jeff_Gore <JefGore@gmail.com>
2022-05-24 21:53:01 -0400

    add yes/no radio button rough styling and functionality

--- src/components/RSVPModal/RSVPModal.jsx
+++ src/components/RSVPModal/RSVPModal.jsx
@@ -3,6 +3,9 @@ import styles from './RSVPModal.module.css'
 
 function RSVPModal ( { closeModal }) {
 
+  const [yesChecked, setYesChecked] = useState(false)
+  const [noChecked, setNoChecked] = useState(false)
+
   const [formData, setFormData] = useState({
     fullName: '',
     numberOfGuests: null,
@@ -10,18 +13,28 @@ function RSVPModal ( { closeModal }) {
     commentsInstructions: ''
   })
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     })
   }
 
-  const handleCheck = e => {
+  const handleCheck = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     })
+  }
+
+  const handleYes = (e) => {
+    setYesChecked(true)
+    setNoChecked(false)
+  }
+
+  const handleNo = (e) => {
+    setYesChecked(false)
+    setNoChecked(true)
   }
 
   const { fullName, numberOfGuests, attending, commentsInstructions} = formData
@@ -31,19 +44,19 @@ function RSVPModal ( { closeModal }) {
   }
 
   return (
-    <div className={`${styles.modalOverlay}`}>
-      <div className={`${styles.modalContainer}`}>
-        <h3 className={`${styles.modalTitle}`}>RSVP</h3>
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalContainer}>
+        <h3 className={styles.modalTitle}>RSVP</h3>
         {/* <h4 className={`${styles.modalContent}`}>the wedding reception of</h4>
         <h4 className={`${styles.modalNames}`}>WILL & ROBYN</h4>
         <h4 className={`${styles.modalTime}`}>SATURDAY, OCTOBER 22, 2022 AT 6:00 PM</h4>
         <h4 className={`${styles.modalLocation}`}>Fanfare</h4>
         <h4 className={`${styles.modalAddress}`}>1103 Manhattan Ave, <br/>Brooklyn, NY 11222</h4> */}
-        <form className={`${styles.modalForm}`}
+        <form className={styles.modalForm}
           autoComplete='off'
         >
-          <label className={`${styles.modalFormLabel}`}>Name
-            <input className={`${styles.modalFormInput}`}
+          <label className={styles.modalFormLabel}>Name
+            <input className={styles.modalFormInput}
               type='text'
               required
               value={fullName}
@@ -51,27 +64,40 @@ function RSVPModal ( { closeModal }) {
               onChange={handleChange}
             />
           </label> 
-          <div className={`${styles.modalAttendingContainer}`}>
-            <h4 className={`${styles.modalAttendingLabel}`}>Attending?</h4>
-            <label className={`${styles.modalAttendingLabel}`}>Yes
-              <input className={`${styles.modalRadioBtn}`}
-                type='radio'
-                value={true}
-                name='attending'
-                onChange={handleCheck}
-                />
-            </label>
-            <label className={`${styles.modalAttendingLabel}`}>No
-              <input className={`${styles.modalRadioBtn}`}
-                type='radio'
-                value={false}
-                name='attending'
-                onChange={handleCheck}
-                />
-            </label> 
+          <div className={styles.modalAttendingContainer}>
+            <h4 className={styles.modalFormLabel}>Attending?</h4>
+            <div className={styles.modalRadioContainer}>
+              <label className={
+                yesChecked ? `${styles.radioYesChecked}` : `${styles.radioYes} `}
+              >Yes
+                <input className={styles.modalRadioBtn}
+                  type='radio'
+                  value={true}
+                  name='attending'
+                  onChange={(e) => {
+                    handleCheck(e)
+                    handleYes(e)
+                  }}
+                  />
+              </label>
+              <label className={
+                noChecked ? `${styles.radioNoChecked}` : `${styles.radioNo} `}>No
+                <input className={styles.modalRadioBtn}
+                  type='radio'
+                  value={false}
+                  setNoChecked={true}
+                  setYesChecked={false}
+                  name='attending'
+                  onChange={(e) => {
+                    handleCheck(e)
+                    handleNo(e)
+                  }}
+                  />
+              </label> 
+            </div>
           </div> 
-          <label className={`${styles.modalFormLabel}`}>Number of guests
-            <input className={`${styles.modalFormInput}`}
+          <label className={styles.modalFormLabel}>Number of guests
+            <input className={styles.modalFormInput}
               type='number'
               required
               value={numberOfGuests}
@@ -79,7 +105,7 @@ function RSVPModal ( { closeModal }) {
               onChange={handleChange}
             />
           </label>
-          <label className={`${styles.modalFormLabel}`}>Comments / Special Instructions
+          <label className={styles.modalFormLabel}>Comments / Special Instructions
             <textarea className={`${styles.modalFormInput} ${styles.modalTextArea}`}
               value={commentsInstructions}
               name='comments'
