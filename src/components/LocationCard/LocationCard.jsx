eb155978902c22bfdcb37ae3232f05769b09983e
Jeff_Gore <JefGore@gmail.com>
2022-05-26 18:12:48 -0400

    style location cards, add locations, refine modal

--- src/components/LocationCard/LocationCard.jsx
+++ src/components/LocationCard/LocationCard.jsx
@@ -8,6 +8,7 @@ const LodgingCard = ({ location, setLocationId }) => {
       <h3 className={`${styles.locationName}`}>{location.name}</h3>
       <Link to={{ pathname: `${location.link}`}} target="_blank" className={`${styles.locationTitle}`}>{location.web}</Link>
       <h4 className={`${styles.locationPhone}`}>{location.phone}</h4>
+      <h4 className={`${styles.locationPrice}`}>{location.price}</h4>
     </div>
   )
 }
--- src/components/LocationList/LocationList.jsx
+++ src/components/LocationList/LocationList.jsx
@@ -4,39 +4,47 @@ const LodgingList = [
     name: "The Marriott",
     web: "bookings",
     link: "https://www.marriott.com/en-us/hotels/nycbk-new-york-marriott-at-the-brooklyn-bridge/overview/",
-    phone: "718-246-7000"
+    phone: "718-246-7000",
+    price: "$$"
   },
   {
     id: 1,
     name: "EVEN Hotel Brooklyn",
     web: "bookings",
     link: "https://www.ihg.com/evenhotels/hotels/us/en/brooklyn/bxyev/hoteldetail?cm_mmc=GoogleMaps-_-VN-_-US-_-BXYEV",
-    phone: "718-552-3800"
+    phone: "718-552-3800",
+    price: "$$"
   },
   {
     id: 2,
+    name: "Ace Hotel Brooklyn",
+    web: "bookings",
+    link: "https://acehotel.com/brooklyn/",
+    phone: "718-313-3636",
+    price: "$$"
+  },
+  {
+    id: 3,
+    name: "Best Western Premier Empire State Hotel",
+    web: "bookings",
+    link: "http://www.bestwestern.com/",
+    phone: "212-576-9000",
+    price: "$$"
+  },
+  {
+    id: 4,
+    name: "The Ludlow New York City",
+    web: "bookings",
+    link: "http://ludlowhotel.com/",
+    phone: "212-432-1818",
+    price: "$$$"
+  },
+  {
+  id: 5,
     name: "airbnb",
     web: "bookings",
     link: "https://www.airbnb.com/s/Brooklyn--New-York--United-States/homes?c=.pi0.pk475441696_25650621256&localized_ghost=true&gclid=Cj0KCQjw1ZeUBhDyARIsAOzAqQLzJNcgWCi_HcY-Flhy9g9VaEYIJGS2Nz3DWKZ5p7Zv85rTlCvu3O0aAiWVEALw_wcBhttps://www.ihg.com/evenhotels/hotels/us/en/brooklyn/bxyev/hoteldetail?cm_mmc=GoogleMaps-_-VN-_-US-_-BXYEV",
     // phone: "555-555-5555"
-  },
-  {
-    id: 3,
-    name: "The Place",
-    web: "bookings",
-    phone: "555-555-5555"
-  },
-  {
-    id: 4,
-    name: "The Place",
-    web: "bookings",
-    phone: "555-555-5555"
-  },
-  {
-    id: 5,
-    name: "The Place",
-    web: "bookings",
-    phone: "555-555-5555"
   }
 ]
 
